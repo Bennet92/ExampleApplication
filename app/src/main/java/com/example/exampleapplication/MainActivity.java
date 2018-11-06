432c18561bf25e548e2fc5e8d75b2933f0cd4b4f
Bennet <bennet92@me.com>
2018-11-06 14:10:16 +0100

    Chapter 2

--- app/src/main/java/com/example/exampleapplication/MainActivity.java
+++ app/src/main/java/com/example/exampleapplication/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-    public void sendMessage(View view){
+    public void sendMessage(View view) throws InterruptedException {
         //log.i(TAG, "index=" + nr);
         EditText editText = (EditText) findViewById(R.id.editText);
         String message = editText.getText().toString();
@@ -29,5 +30,23 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
+        MainActivity.this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    warten();
+                    Button button = (Button) findViewById(R.id.button);
+                    button.setText("Gedrückt!");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    public void warten() throws InterruptedException {
+        synchronized (this){
+            wait(5000);
+        }
     }
 }
