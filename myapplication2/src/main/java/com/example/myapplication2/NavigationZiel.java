23f01f4962b30159ef56a90039f5935d3880d926
Marius Neubert <marius.neubert@telekom.de>
2018-11-07 11:45:48 +0100

    3.3 Fertig inkl 3.3.4

---
+++ myapplication2/src/main/java/com/example/myapplication2/NavigationZiel.java
@@ -0,0 +1,25 @@
+package com.example.myapplication2;
+
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NavigationZiel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation_ziel);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
